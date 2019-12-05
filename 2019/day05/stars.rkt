548dbc7eb1149b9ce9f1f12e8c9a2f3b9d862f1b
yfzhe <yfangzhe@gmail.com>
2019-12-05 16:15:43 +0800

    fix and update
    maybe I need typed-racket tomorrow

--- 2019/day05/stars.rkt
+++ 2019/day05/stars.rkt
@@ -3,7 +3,7 @@
 (module+ test
   (require rackunit))
 
-(define (parse-opcode+modes value)
+(define (parse-modes+opcode value)
   (quotient/remainder value 100))
 
 (define (get-modes modes idx)
@@ -18,68 +18,74 @@
 
 ;; Program: (listof Integer)
 
-;; prog-read: Mode * Index * Program -> Integer
-(define (prog-read mode input-idx prog)
+;; prog-read: Program * Mode * Index -> Integer
+(define (prog-read prog mode input-idx)
   (case mode
     [(0) ; position mode
      (list-ref prog (list-ref prog input-idx))]
     [(1) ; immedrate mode
      (list-ref prog input-idx)]))
 
-;; write-result: Mode * Index * Integer * Program -> Prog
-(define (prog-write mode idx val prog)
+;; write-result: Program * Mode * Index * Integer -> Prog
+(define (prog-write prog mode idx val)
   (case mode
     [(0) ; position mode
      (list-set prog (list-ref prog idx) val)]))
 
-;; run-program: Program -> Input -> final output
+(define (prog-read* prog modes base-pos offset)
+  (prog-read prog
+             (get-modes modes (sub1 offset))
+             (+ base-pos offset)))
+
+(require racket/trace)
+
 (define (run-program prog input)
   (let loop ([prog prog] [pos 0] [input input] [output '()])
-    (define-values (opcode modes)
-      (parse-opcode+modes (list-ref prog pos)))
+    (define-values (modes opcode)
+      (parse-modes+opcode (list-ref prog pos)))
     (case opcode
-      [(99) (car output)]
-      [(1 2)
-       (define operand0 (prog-read (get-modes modes 0) (+ pos 1) prog))
-       (define operand1 (prog-read (get-modes modes 1) (+ pos 2) prog))
+      [(99)
+       (car output)]
+      [(1 2) ; plus / multiply
+       (define operand0 (prog-read* prog modes pos 1))
+       (define operand1 (prog-read* prog modes pos 2))
 
-       (define op (if (= opcode 1) + *))
+       (define op (case opcode [(1) +] [(2) *]))
        (define result (op operand0 operand1))
 
-       (loop (prog-write (get-modes modes 2) (+ pos 3) result prog)
+       (loop (prog-write prog (get-modes modes 2) (+ pos 3) result)
              (+ pos 4)
              input
              output)]
-      [(3)
-       (define result-pos (list-ref prog (+ pos 1)))
-       (loop (prog-write 0 (+ pos 1) input prog)
+      [(3) ; read from input
+       (loop (prog-write prog 0 (+ pos 1) input)
              (+ pos 2)
              #f
              output)]
-      [(4)
+      [(4) ; write to output
        (loop prog
              (+ pos 2)
              input
-             (cons (prog-read 0 (+ pos 1) prog)
+             (cons (prog-read prog 0 (+ pos 1))
                    output))]
-      [(5 6)
-       (define operand0 (prog-read (get-modes modes 0) (+ pos 1) prog))
-       (define operand1 (prog-read (get-modes modes 1) (+ pos 2) prog))
-
-       (if (if (= opcode 6) (zero? operand0) (not (zero? operand0)))
-           (loop prog operand1 input output)
-           (loop prog (+ pos 3) input output))]
-      [(7 8)
-       (define operand0 (prog-read (get-modes modes 0) (+ pos 1) prog))
-       (define operand1 (prog-read (get-modes modes 1) (+ pos 2) prog))
+      [(5 6) ; jump-if-true / jump-if-false
+       (define operand0 (prog-read* prog modes pos 1))
+       (define operand1 (prog-read* prog modes pos 2))
 
        (define op
-         (if (= opcode 7) < =))
-
+         (case opcode [(5) (compose not zero?)] [(6) zero?]))
        (define result
-         (if (op operand0 operand1) 1 0))
+         (if (op operand0) operand1 (+ pos 3)))
 
-       (loop (prog-write 0 (+ pos 3) result prog)
+       (loop prog result input output)]
+      [(7 8) ; less than / equals
+       (define operand0 (prog-read* prog modes pos 1))
+       (define operand1 (prog-read* prog modes pos 2))
+
+       (define op (case opcode [(7) <] [(8) =]))
+       (define result (if (op operand0 operand1) 1 0))
+
+       (loop (prog-write prog 0 (+ pos 3) result)
              (+ pos 4)
              input
              output)])))
