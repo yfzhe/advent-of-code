01e4595c9ad44e4868cd9d4baa75826833db1486
yfzhe <yfangzhe@gmail.com>
2025-12-06 00:29:26 +0800

    improve 2025 day 1

--- 2025/day01/main.rkt
+++ 2025/day01/main.rkt
@@ -3,61 +3,56 @@
 (module+ test
   (require rackunit))
 
-(define M 100)
-
-;; Roration ::= (L <num>) | (R <num>)
-
 (define (parse-input in)
   (for/list ([line (in-lines in)])
-    (list (string->symbol (substring line 0 1))
-          (string->number (substring line 1)))))
+    (parse-line line)))
 
-;; rotate : N * Rotation -> (Values N Nat)
+;; use an integer to represent a rotation
+;; positive as turning Right, negative as turning Left
+;; inspired by https://github.com/mstksg/advent-of-code/wiki/Reflections-2025#day-1
+(define (parse-line line)
+  (define dir (string-ref line 0))
+  (* (match (string-ref line 0) [#\L -1] [#\R 1])
+     (string->number (substring line 1))))
 
-(define (rotate/1 base rotation)
-  (let ([result
-         (match rotation
-           [`(L ,num) (modulo (- base num) M)]
-           [`(R ,num) (modulo (+ base num) M)])])
-    (values result
-            (if (zero? result) 1 0))))
+;; ---------- PART 1 ----------
+(define (rotate/1 base offset)
+  (define result (modulo (+ base offset) 100))
+  (values result (if (zero? result) 1 0)))
 
-(define (solve rotate rotations)
+(define (solve rotate offsets)
   (for/fold ([base 50]
              [cnt 0]
              #:result cnt)
-            ([rotation (in-list rotations)])
+            ([rotation (in-list offsets)])
     (define-values (result 0s) (rotate base rotation))
     (values result (+ cnt 0s))))
 
 (module+ test
   (define input
-    (parse-input (open-input-file "test")))
+    (call-with-input-file "test" parse-input))
 
   (check-equal? (solve rotate/1 input) 3))
 
 (module+ main
   (define input
-    (parse-input (open-input-file "input")))
+    (call-with-input-file "input" parse-input))
 
   (solve rotate/1 input))
 
 ;;; ---------- PART 2 ----------
 
-(define (rotate/2 base rotation)
-  (match rotation
-    [`(L ,num)
-     (define result (- base num))
-     (values (modulo result M)
-             (- (floor (/ (sub1 base) M)) (floor (/ (sub1 result) M))))]
-    [`(R ,num)
-     (define result (+ base num))
-     (values (modulo result M)
-             (- (floor (/ result M)) (floor (/ base M))))]))
+(define (rotate/2 base offset)
+  (define result (+ base offset))
+  (define 0s
+    (if (> offset 0)
+        (- (floor (/ result 100)) (floor (/ base 100)))
+        (- (floor (/ (sub1 base) 100)) (floor (/ (sub1 result) 100)))))
+  (values (modulo result 100) 0s))
 
 (module+ test
   (check-equal? (solve rotate/2 input) 6)
-  (check-equal? (solve rotate/2 '((R 1000))) 10))
+  (check-equal? (solve rotate/2 '(1000)) 10))
 
 (module+ main
   (solve rotate/2 input))
